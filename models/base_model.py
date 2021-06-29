08fd6bc0218fd01edb3c37ec7228e1404a71609b
Ignacio Chitnisky <elinmortal@gmail.com>
2021-06-29 23:17:51 +0000

    finished 3

---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""BaseModel class"""
+
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    """BaseModel defines all common attributes/methods for other classes"""
+
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        newdict = self.__dict__.copy()
+        newdict["__class__"] = self.__class__.__name__
+        newdict["created_at"] = self.created_at.isoformat()
+        newdict["updated_at"] = self.updated_at.isoformat()
+        return newdict
